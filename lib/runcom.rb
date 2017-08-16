591172e3a7d35f73345349b81bcb41c3bc197787
Brooke Kuhlmann <brooke@alchemists.io>
2017-08-15 20:21:04 -0600

    Added base error.
    Will be used to wrap all errors issued by this gem.

--- lib/runcom.rb
+++ lib/runcom.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 require "runcom/identity"
+require "runcom/errors/base"
 require "runcom/xdg/configuration"
 require "runcom/configuration"
---
+++ lib/runcom/errors/base.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Runcom
+  module Errors
+    # The default class for all gem related errors.
+    class Base < StandardError
+    end
+  end
+end
