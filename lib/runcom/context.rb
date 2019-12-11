7bd534a374e5b77a469bc4837dca260f5f8043eb
Brooke Kuhlmann <brooke@alchemists.io>
2019-12-10 19:28:49 -0700

    Added context.
    Necessary to encapsulate the context in which cache, config, and data
    objects are to be used. Provides safe defaults to avoid customization
    unless needed.

---
+++ lib/runcom/context.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Runcom
+  # A common context for all XDG custom objects.
+  Context = Struct.new :defaults, :home, :environment, :xdg, keyword_init: true do
+    def initialize *arguments
+      super
+
+      self[:home] ||= Paths::Home
+      self[:environment] ||= ENV
+      self[:xdg] = xdg.new home: home, environment: environment if xdg
+    end
+  end
+end
---
+++ spec/lib/runcom/context_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Runcom::Context do
+  describe "#initialize" do
+    it "answers default context" do
+      expect(described_class.new).to have_attributes(
+        home: Runcom::Paths::Home,
+        environment: ENV,
+        xdg: nil
+      )
+    end
+
+    it "answers custom context" do
+      context = described_class.new home: XDG::Paths::Standard,
+                                    environment: {b: 2},
+                                    xdg: XDG::Config
+
+      expect(context).to have_attributes(
+        home: XDG::Paths::Standard,
+        environment: {b: 2},
+        xdg: XDG::Config
+      )
+    end
+  end
+end
