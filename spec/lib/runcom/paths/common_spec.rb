2e7bffae543d9b95f1d07ae228c0b1f67d31ff30
Brooke Kuhlmann <brooke@alchemists.io>
2022-01-30 08:24:33 -0700

    Refactored common path spec subject and let statements
    Necessary to make this spec consistent in naming and structure with
    what is found in the home specs.

--- spec/lib/runcom/paths/common_spec.rb
+++ spec/lib/runcom/paths/common_spec.rb
@@ -5,12 +5,13 @@ require "spec_helper"
 RSpec.describe Runcom::Paths::Common do
   using Refinements::Pathnames
 
-  subject(:custom) { described_class.new path, context: }
+  subject :path do
+    described_class.new test_path, context: Runcom::Context.new(xdg: XDG::Data, environment:)
+  end
 
   include_context "with temporary directory"
 
-  let(:path) { "test/example.txt" }
-  let(:context) { Runcom::Context.new xdg: XDG::Data, environment: }
+  let(:test_path) { "test/example.txt" }
 
   let :environment do
     {
@@ -22,85 +23,85 @@ RSpec.describe Runcom::Paths::Common do
 
   describe "#relative" do
     it "answers relative path with string" do
-      expect(custom.relative).to eq(Pathname("test/example.txt"))
+      expect(path.relative).to eq(Pathname("test/example.txt"))
     end
 
     context "with empty string" do
-      let(:path) { "" }
+      let(:test_path) { "" }
 
       it "answers empty path" do
-        expect(custom.relative).to eq(Pathname(""))
+        expect(path.relative).to eq(Pathname(""))
       end
     end
 
     context "with nil" do
-      let(:path) { nil }
+      let(:test_path) { nil }
 
       it "fails with type error" do
-        expectation = -> { custom.relative }
+        expectation = -> { path.relative }
         expect(&expectation).to raise_error(TypeError, /conversion of nil/)
       end
     end
 
     context "with pathname" do
-      let(:path) { Pathname "test/example.txt" }
+      let(:test_path) { Pathname "test/example.txt" }
 
       it "answers relative path" do
-        expect(custom.relative).to eq(Pathname("test/example.txt"))
+        expect(path.relative).to eq(Pathname("test/example.txt"))
       end
     end
   end
 
   describe "#namespace" do
-    it "answers namespace with relative path" do
-      expect(custom.namespace).to eq(Pathname("test"))
+    it "answers parent directory with relative path" do
+      expect(path.namespace).to eq(Pathname("test"))
     end
 
     context "with empty path" do
-      let(:path) { "" }
+      let(:test_path) { "" }
 
-      it "answers namespace" do
-        expect(custom.namespace).to eq(Pathname(".."))
+      it "answers parent directory" do
+        expect(path.namespace).to eq(Pathname(".."))
       end
     end
   end
 
   describe "#file_name" do
     it "answers file name" do
-      expect(custom.file_name).to eq(Pathname("example.txt"))
+      expect(path.file_name).to eq(Pathname("example.txt"))
     end
 
     context "with empty path" do
-      let(:path) { "" }
+      let(:test_path) { "" }
 
       it "answers empty file name" do
-        expect(custom.file_name).to eq(Pathname(""))
+        expect(path.file_name).to eq(Pathname(""))
       end
     end
   end
 
   describe "#current" do
     it "answers path when path exists" do
-      file_path = temp_dir.join("test/example.txt").make_path
-      expect(custom.current).to eq(file_path)
+      file_path = temp_dir.join("test").make_path.join("example.txt").touch
+      expect(path.current).to eq(file_path)
     end
 
     it "answers nil when path doesn't exist" do
-      expect(custom.current).to eq(nil)
+      expect(path.current).to eq(nil)
     end
 
     context "with empty path" do
-      let(:path) { "" }
+      let(:test_path) { "" }
 
       it "answers home directory" do
-        expect(custom.current).to eq(temp_dir)
+        expect(path.current).to eq(temp_dir)
       end
     end
   end
 
   describe "#all" do
     it "answers paths with namespace and file path" do
-      expect(custom.all).to contain_exactly(
+      expect(path.all).to contain_exactly(
         temp_dir.join("test", "example.txt"),
         temp_dir.join("one", "test", "example.txt"),
         temp_dir.join("two", "test", "example.txt")
@@ -108,10 +109,10 @@ RSpec.describe Runcom::Paths::Common do
     end
 
     context "with namespace only" do
-      let(:path) { "test" }
+      let(:test_path) { "test" }
 
       it "answers paths with namespace only" do
-        expect(custom.all).to contain_exactly(
+        expect(path.all).to contain_exactly(
           temp_dir.join("test"),
           temp_dir.join("one", "test"),
           temp_dir.join("two", "test")
@@ -120,21 +121,17 @@ RSpec.describe Runcom::Paths::Common do
     end
 
     context "with empty path" do
-      let(:path) { "" }
+      let(:test_path) { "" }
 
       it "answes something" do
-        expect(custom.all).to contain_exactly(
-          temp_dir,
-          temp_dir.join("one"),
-          temp_dir.join("two")
-        )
+        expect(path.all).to contain_exactly(temp_dir, temp_dir.join("one"), temp_dir.join("two"))
       end
     end
   end
 
   describe "#inspect" do
     it "answers environment settings" do
-      expect(custom.inspect).to eq(
+      expect(path.inspect).to eq(
         "XDG_DATA_HOME=#{temp_dir} XDG_DATA_DIRS=#{temp_dir}/one:#{temp_dir}/two"
       )
     end
