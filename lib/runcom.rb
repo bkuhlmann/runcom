cdd31face7f386b8250d470af558eeac5f659e63
Brooke Kuhlmann <brooke@alchemists.io>
2023-09-30 15:48:11 -0600

    Fixed Zeitwerk loader
    Necessary to ensure the gem is properly tagged and the default inflector is used since the gem inflector unfortunately looks for a version file which is not used by this gem.
    
    Milestone: patch

--- lib/runcom.rb
+++ lib/runcom.rb
@@ -3,7 +3,11 @@
 require "xdg"
 require "zeitwerk"
 
-Zeitwerk::Loader.for_gem.setup
+Zeitwerk::Loader.new.then do |loader|
+  loader.tag = File.basename __FILE__, ".rb"
+  loader.push_dir __dir__
+  loader.setup
+end
 
 # Main namespace.
 module Runcom
